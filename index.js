f3fe0b12a12318bb713d0fa0d870f95badd0dc60
Alain Léglise <prestataire.alain.leglise@sam-assurance.com>
2022-06-14 17:05:26 +0200

    Création d'un module de génération de mot de passe utilisant la librairie "generate-password"

---
+++ index.js
@@ -0,0 +1,13 @@
+const generator = require('generate-password');
+
+const passwords = generator.generateMultiple(process.argv[2], {
+	length: 32,
+	numbers: true,
+	symbols: true,
+	strict: true,
+	excludeSimilarCharacters: true
+});
+
+passwords.forEach(password => {
+console.log(password);
+});
