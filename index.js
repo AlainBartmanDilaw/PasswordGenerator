ec2cffe520ab826348a2615bb0e5fcbe2d6ce647
Alain Léglise <prestataire.alain.leglise@sam-assurance.com>
2022-06-15 14:07:45 +0200

    Affiche 1 password par défaut et N si un argument (numérique non testé) est passé en paramètre

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const generator = require('generate-password');
 
-const passwords = generator.generateMultiple(process.argv[2], {
 	length: 32,
 	numbers: true,
 	symbols: true,
 	strict: true,
 	excludeSimilarCharacters: true
+let _count = 1;
+if (process.argv.length === 2) {
+	let _count = process.argv[2];
+}
+
+const passwords = generator.generateMultiple(_count, {
 });
 
 passwords.forEach(password=>{
