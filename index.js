ecb4415d54c5b2d6543fd64713be2769c2170840
Alain Léglise <prestataire.alain.leglise@sam-assurance.com>
2022-06-15 14:09:41 +0200

    Correction du module suite désorganisation des lignes

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const generator = require('generate-password');
 
-	length: 32,
-	numbers: true,
-	symbols: true,
-	strict: true,
-	excludeSimilarCharacters: true
 let _count = 1;
 if (process.argv.length === 2) {
-	let _count = process.argv[2];
+    let _count = process.argv[2];
 }
 
 const passwords = generator.generateMultiple(_count, {
+    length: 32,
+    numbers: true,
+    symbols: true,
+    strict: true,
+    excludeSimilarCharacters: true
 });
 
 passwords.forEach(password=>{
